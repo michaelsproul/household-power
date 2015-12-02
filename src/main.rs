ac99cc181f1d2f493f63a3fb074dfb7a73104d70
Michael Sproul <micsproul@gmail.com>
2015-12-02 12:15:05 +1100

    Update lib usage and modernise error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
 extern crate xml;
 extern crate serial;
-extern crate time;
 #[macro_use] extern crate log;
 extern crate festivus_client;
 
 use std::path::Path;
 use std::io::Read;
 use std::collections::HashMap;
-use std::error::Error as ErrorT;
+use std::error::Error;
 
 use xml::EventReader;
-use xml::reader::events::XmlEvent;
-use xml::reader::events::XmlEvent::*;
+use xml::reader::XmlEvent;
+use xml::reader::XmlEvent::*;
+use xml::reader::Error as XmlError;
 use xml::name::OwnedName;
 
 use serial::prelude::*;
 use serial::posix::TTYPort;
 use serial::PortSettings;
 use serial::BaudRate::*;
-use time::Duration;
+use std::time::Duration;
 
 use festivus_client::Festivus;
 
 use Parser::*;
 
-type Error = Box<ErrorT>;
+/// Convert a String to a Box<Error>.
+fn string_error<T>(s: String) -> Result<T, Box<Error>> {
+    let err: Box<Error + Send + Sync> = From::from(s);
+    Err(err as Box<Error>)
+}
 
 enum Parser {
     Top(&'static str, Vec<Parser>),
@@ -42,29 +46,34 @@ impl Parser {
 
 trait EventReaderExt {
     /// Next important tag.
-    fn next_tag(&mut self) -> XmlEvent;
+    fn next_tag(&mut self) -> Result<XmlEvent, XmlError>;
     /// Consume tags until the given end tag is reached.
     fn read_to_tag_end(&mut self, tag: &str);
 }
 
 impl<T> EventReaderExt for EventReader<T> where T: Read {
-    fn next_tag(&mut self) -> XmlEvent {
-        match self.next() {
-            // Ignored tag types.
-            StartDocument { .. } |
-            ProcessingInstruction { .. } |
-            CData(..) |
-            Comment(..) |
-            Whitespace(..) => self.next_tag(),
-            x => {
-                info!("Read tag: {:?}", x);
-                x
+    fn next_tag(&mut self) -> Result<XmlEvent, XmlError> {
+        self.next().and_then(|tag| {
+            match tag {
+                // Ignored tag types.
+                StartDocument { .. } |
+                ProcessingInstruction { .. } |
+                CData(..) |
+                Comment(..) |
+                Whitespace(..) => self.next_tag(),
+                // Anything else (not ignored).
+                x => {
+                    info!("Read tag: {:?}", x);
+                    Ok(x)
+                }
             }
-        }
+        })
     }
+
     fn read_to_tag_end(&mut self, tag: &str) {
         loop {
-            if let EndElement { ref name, .. } = self.next_tag() {
+            // FIXME: infinite loop on error?
+            if let Ok(EndElement { ref name, .. }) = self.next_tag() {
                 if &name.local_name[..] == tag {
                     info!("Closed </{}>", tag);
                     break;
@@ -85,28 +94,18 @@ fn name_matches_str(name: &OwnedName, str_name: &str) -> bool {
     &name.local_name == str_name
 }
 
-trait ToResult {
-    fn to_result(self) -> Result<(), ()>;
-}
-impl ToResult for bool {
-    fn to_result(self) -> Result<(), ()> {
-        match self {
-            true => Ok(()),
-            false => Err(())
-        }
-    }
-}
-
-// Responsibilities: parsers are responsible for parsing the *inside and end* of their tag,
+// Parsers are responsible for parsing the *inside and end* of their tag,
 // having had their start parsed by their parent element. The exception to this is
 // the `Top` tag which parses its own start.
-fn run_parser<T>(src: &mut EventReader<T>, parser: &Parser)
-    -> Result<HashMap<&'static str, String>, ()>
-    where T: Read
+fn run_parser<T: Read>(src: &mut EventReader<T>, parser: &Parser)
+    -> Result<HashMap<&'static str, String>, Box<Error>>
 {
     match *parser {
         Top(tag, ref subparsers) => {
-            try!(is_start_tag(&src.next_tag(), tag).to_result());
+            let xml_ev = try!(src.next_tag());
+            if !is_start_tag(&xml_ev, tag) {
+                return string_error(format!("Matching start tag not found for Top parser: {}", tag));
+            }
 
             // Parse the inside and end of the tag.
             parse_tag(src, tag, subparsers)
@@ -117,8 +116,8 @@ fn run_parser<T>(src: &mut EventReader<T>, parser: &Parser)
         Contents(tag, key_name) => {
             let mut result = HashMap::new();
             match src.next_tag() {
-                Characters(tag_content) => { result.insert(key_name, tag_content); },
-                _ => return Err(())
+                Ok(Characters(tag_content)) => { result.insert(key_name, tag_content); },
+                _ => return string_error(format!("Tag contents not found for tag parser"))
             }
             src.read_to_tag_end(tag);
             Ok(result)
@@ -126,9 +125,8 @@ fn run_parser<T>(src: &mut EventReader<T>, parser: &Parser)
     }
 }
 
-fn parse_tag<T>(src: &mut EventReader<T>, tag: &'static str, subparsers: &[Parser])
-    -> Result<HashMap<&'static str, String>, ()>
-    where T: Read
+fn parse_tag<T: Read>(src: &mut EventReader<T>, tag: &'static str, subparsers: &[Parser])
+    -> Result<HashMap<&'static str, String>, Box<Error>>
 {
     let mut result = HashMap::new();
     for subparser in subparsers {
@@ -136,14 +134,12 @@ fn parse_tag<T>(src: &mut EventReader<T>, tag: &'static str, subparsers: &[Parse
         // Loop through tokens until a match for this subparser is found.
         loop {
             match src.next_tag() {
-                StartElement { name: ref tag_name, .. } => {
+                Ok(StartElement { name: ref tag_name, .. }) => {
                     // Tag matches sub-parser.
                     if name_matches_str(tag_name, subparser.tag_name()) {
                         debug!("Matched <{}>", subparser.tag_name());
-                        match run_parser(src, subparser) {
-                            Ok(subresult) => { result.extend(subresult); },
-                            Err(()) => return Err(())
-                        }
+                        let subresult = try!(run_parser(src, subparser));
+                        result.extend(subresult);
                         break;
                     }
                     // Otherwise, skip the tag.
@@ -151,7 +147,7 @@ fn parse_tag<T>(src: &mut EventReader<T>, tag: &'static str, subparsers: &[Parse
                         src.read_to_tag_end(&tag_name.local_name)
                     }
                 }
-                _ => return Err(())
+                _ => return string_error(format!("XML stream out of sync with parser"))
             }
         }
     }
@@ -160,18 +156,18 @@ fn parse_tag<T>(src: &mut EventReader<T>, tag: &'static str, subparsers: &[Parse
     Ok(result)
 }
 
-fn init_serial() -> Result<TTYPort, Error> {
+fn init_serial() -> Result<TTYPort, Box<Error>> {
     let settings = PortSettings {
         baud_rate: Baud57600,
         ..PortSettings::default()
     };
     let mut port = try!(TTYPort::open(Path::new("/dev/ttyUSB0")));
     try!(port.configure(&settings));
-    try!(port.set_timeout(Duration::days(1)));
+    try!(port.set_timeout(Duration::new(10 /* seconds */, 0)));
     Ok(port)
 }
 
-fn main_with_result() -> Result<(), Error> {
+fn main_with_result() -> Result<(), Box<Error>> {
     let serial_input = try!(init_serial());
 
     let mut event_reader = EventReader::new(serial_input);
@@ -181,7 +177,7 @@ fn main_with_result() -> Result<(), Error> {
             Contents("time", "time"),
             Contents("tmpr", "temperature"),
             Tag("ch1", vec![Contents("watts", "total")]),
-            Tag("ch2", vec![Contents("watts", "hotwater")]),
+            Tag("ch2", vec![Contents("watts", "hot_water")]),
             Tag("ch3", vec![Contents("watts", "solar")])
         ]);
 
